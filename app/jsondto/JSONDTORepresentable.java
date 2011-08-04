c7d755b809496cdc3e8c2b4bb3df45723b52d72a
Jarno Rantanen <jarno.rantanen@futurice.com>
2011-08-04 16:24:24 +0300

    Added an interface that marks Models JSON-representable.

---
+++ app/jsondto/JSONDTORepresentable.java
@@ -0,0 +1,24 @@
+package jsondto;
+
+/**
+ * Model objects marked with this interface are able to represent themselves as
+ * a JSON-DTO, and vice-versa (that is, able to set their state according to a
+ * given DTO).
+ * 
+ * @author Jarno Rantanen <jarno@jrw.fi>
+ */
+public interface JSONDTORepresentable<DTO extends JSONDTO> {
+
+	/**
+	 * Copies any state in the given JSON-DTO to this model object.
+	 * 
+	 */
+	public void merge(DTO dto);
+	
+	/**
+	 * Returns a JSON-DTO-representation of this model object.
+	 * 
+	 */
+	public DTO toDTO();
+	
+}
