c8cca3ab671b4a63df65c1065ab0b40e61836a71
Jarno Rantanen <jarno.rantanen@futurice.com>
2011-08-04 16:43:23 +0300

    Added a utility method for rendering a JSONDTORepresentable object out as JSON.

--- app/jsondto/Util.java
+++ app/jsondto/Util.java
@@ -4,6 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import play.mvc.Http.Response;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Utility methods for the JSON-DTO module.
@@ -13,6 +20,7 @@ import java.io.InputStreamReader;
 public abstract class Util {
 
 	public static final String CHARSET = "UTF-8";
+	public static final String CONTENT_TYPE = "application/json; charset=utf-8";
 
 	/**
 	 * Helper method for converting a stream to a string.
@@ -42,6 +50,50 @@ public abstract class Util {
 		}
 
 		return stringBuilder.toString();
+
+	}
+
+	/**
+	 * Renders the given JSON-representable Model object as JSON to the given
+	 * request's body.
+	 * 
+	 * Also accepts a List of such objects, in which case a JSON list is
+	 * prepared for output.
+	 * 
+	 * TODO: Allow passing a JSONDTO directly as well..?
+	 * 
+	 * @throws Exception
+	 */
+	public static void renderDTO(Object dtos, Response response) throws Exception {
+
+		Gson gson = new GsonBuilder().create();
+		String jsonString;
+
+		if (dtos instanceof JSONDTORepresentable) {
+
+			jsonString = gson.toJson(((JSONDTORepresentable) dtos).toDTO());
+
+		} else if (dtos instanceof List) {
+
+			ArrayList a = new ArrayList();
+
+			for (Object dto : (List) dtos)
+
+				if (dto instanceof JSONDTORepresentable)
+					a.add(((JSONDTORepresentable) dto).toDTO());
+				else
+					throw new IllegalArgumentException("Expecting a List of JSONDTORepresentable instances, " + dto.getClass() + " encountered");
+
+			jsonString = gson.toJson(a);
+
+		} else {
+
+			throw new IllegalArgumentException("Expecting (a list of) JSONDTORepresentable instances, " + dtos.getClass() + " encountered");
+
+		}
+
+		response.contentType = CONTENT_TYPE;
+		response.out.write(jsonString.getBytes(CHARSET));
 
 	}
 
